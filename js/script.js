7ded6b8c481641682459c65ec1dba36214bcfb12
jewon3309@gmail.com <jewon3309@gmail.com>
2024-12-27 22:00:55 +0900

    최종 다운로드 이미지 폰트 스타일, 크기, 이미지 사이즈 수정

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@
     - 작업자: 김제원, 김예빈
 */ 
 
-const btn_1 = document.getElementById('btn-1');
-const btn_2 = document.getElementById('btn-2');
-const btn_3 = document.getElementById('btn-3');
+const btn_1 = document.getElementById('mainBtn');
+const btn_2 = document.getElementById('nameBtn');
+const btn_3 = document.getElementById('resultBtn');
 
 // 화면 전환
 const contents_1 = document.getElementById('contents-1');
@@ -50,32 +50,28 @@ const downloadCardText = document.getElementById("downloadCardText");
 
 // 백그라운드 스몰 말씀 카드 이미지 목록
 const backgroundImages = [
-    // star
+    // small card icon
     'images/card/1-1_sw_01539D.png',
     'images/card/1-2_sb_FFB2C2.png',
     'images/card/1-3_sb_96E1C0.png',
 
-    // cloud
     'images/card/2-1_sw_5E3F82.png', 
     'images/card/2-2_sw_F87839.png', 
     'images/card/2-3_sw_3DC0A0.png', 
 
-    // grass
     'images/card/3-1_sw_98BC62.png', 
     'images/card/3-2_sw_365260.png', 
     'images/card/3-3_sw_A09071.png', 
     
-    // cloud(?)
     'images/card/4-1_sw_F182A2.png', 
     'images/card/4-2_sb_F5C8A9.png', 
     'images/card/4-3_sw_C83B44.png', 
     
-    // rain
     'images/card/5-1_sw_00203F.png', 
     'images/card/5-2_sw_555A46.png', 
     'images/card/5-3_sb_FFD55F.png',
 
-    // large icon
+    // large card icon
     'images/card/6-1_lb_FCFCE4.png',
     'images/card/6-2_lb_FFDF84.png',
     'images/card/6-3_lb_FFFDB7.png',
@@ -95,7 +91,6 @@ const backgroundImages = [
     'images/card/10-1_lw_A1B5D8.png',
     'images/card/10-2_lw_A896C8.png',
     'images/card/10-3_lb_96E1C0.png'
-
 ];
 
 //  화면 전환 클릭 이벤트
@@ -124,7 +119,7 @@ btn_2.addEventListener('click', () => {
         contents_2.classList.remove('active');
         contents_3.classList.add('active');
         
-        setTimeout(() => {  // 3초 후 이미지 다운로드 페이지로 전환
+        setTimeout(() => {  
             contents_3.classList.remove('active');
             contents_4.classList.add('active');
             clearInterval(changeImgLog);
@@ -132,7 +127,6 @@ btn_2.addEventListener('click', () => {
     }
 })
 
-// 말씀 다시 뽑기 이벤트
 reBtn.addEventListener('click', () => {
     reBtn.classList.add("clicked");
 
@@ -147,9 +141,10 @@ reBtn.addEventListener('click', () => {
     cardImg.style.left = "0px";
     cardImg.style.top = "0px";
 
-    setTimeout(() => {  // 3초 후 이미지 다운로드 페이지로 전환
+    setTimeout(() => {  
         reBtn.classList.remove("clicked");
-
+        
+        // init
         smallCardIcon.src = '';
         smallCardIcon.style.cssText = '';
         downloadBackImg.style.cssText = '';
@@ -175,14 +170,14 @@ downloadBtn.addEventListener('click', async () => {
     contents_5.classList.add('active');
 
     try {
-        const canvas = await html2canvas(downloadImg, { useCORS: true});
+        const canvas = await html2canvas(downloadImg, {useCORS: true});
         const imageData = canvas.toDataURL("image/png");
         const resultImg = document.getElementById("resultImg");
         resultImg.src = imageData;
 
         const link = document.createElement("a");
         link.href = imageData;
-        link.download = `말씀카드_${nameInput.value}.png`; // 파일명 설정
+        link.download = `bibleCard_${nameInput.value}.png`; // 파일명 설정
 
         const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
         const isAndroid = /Android/i.test(navigator.userAgent);
@@ -191,14 +186,13 @@ downloadBtn.addEventListener('click', async () => {
             contents_5.classList.remove('active');
             contents_6.classList.add('active');
             link.target = '_blank'; // 새 창에서 열기
-            // alert("이미지를 길게 눌러 저장하세요😇");
+            
         } else if (isAndroid) {
             contents_5.classList.remove('active');
             contents_6.classList.add('active');
             try {
                 setTimeout(() => link.click(), 50); // 다운로드 시도
             } catch (err) {
-                // alert("이미지를 길게 눌러 저장하세요😇");
                 console.log(err);
             }
         } else {
@@ -207,13 +201,9 @@ downloadBtn.addEventListener('click', async () => {
     } catch (err) {
         console.error("캡처 실패:", err);
     }
-
-    // 메모리 누수를 방지하기 위해 URL 해제
-    setTimeout(() => URL.revokeObjectURL(link.href), 5000);
 });
 
 nameInput.addEventListener('input', () => {
-    
     let nameValue = nameInput.value;
     let realLength = [...nameValue].length;
 
@@ -229,7 +219,6 @@ nameInput.addEventListener('input', () => {
         nameInput.value = nameValue
         realLength = maxLength
     }
-
     nameCount.textContent = `(${realLength}/${maxLength})`;
 })
 
@@ -258,13 +247,11 @@ async function getBibleVerses() {
     }
 }
 
-// 랜덤 요소 선택 함수
-function getRandomElement(array) {
+function getRandomElement(array) { // 리스트 내 랜덤 추출
     return array[Math.floor(Math.random() * array.length)];
 }
 
-// 이미지 셋팅
-async function getSmallCard(){
+async function getSmallCard(){ // 성경 말씀, 이미지 랜덤 추출
     const verses = await getBibleVerses();
     const randomVerse = getRandomElement(verses);
     const randomBackground = getRandomElement(backgroundImages);
@@ -277,15 +264,15 @@ async function getSmallCard(){
 function getBackgroundType(backgroundPath) {
 
     if (backgroundPath.includes('sw')) {
-        return { isType2Layout: true, isBlackText: false };  // 타입 1: 로고o, 흰색
+        return { isType2Layout: true, isBlackText: false }; // small icon / white font
     } else if (backgroundPath.includes('sb')) {
-        return { isType2Layout: true, isBlackText: true };   // 타입 2: 로고o, 검정색
+        return { isType2Layout: true, isBlackText: true }; // small icon / black font
     } else if (backgroundPath.includes('lw')) {
-        return { isType2Layout: false, isBlackText: false }; // 타입 3: 로고x, 흰색
+        return { isType2Layout: false, isBlackText: false }; // large icon / white font
     } else if (backgroundPath.includes('lb')) {
-        return { isType2Layout: false, isBlackText: true };  // 타입 4: 로고x, 검정색
+        return { isType2Layout: false, isBlackText: true }; // large icon / black font
     }
-    return { isType2Layout: false, isBlackText: false }; // 기본값
+    return { isType2Layout: false, isBlackText: false };
 }
 
 function setBibleCard(userName) {
@@ -305,10 +292,7 @@ function setBibleCard(userName) {
         downloadCardBible.innerHTML = bibleContents;
         downloadCardFooter.innerHTML = bibleLoc;
 
-        if (fontType.isType2Layout) {// Logo top 112 bottum 45
-
-            cardBible.style.marginBottom = "5%";
-            downloadCardBible.style.marginTop = "10%";
+        if (fontType.isType2Layout) { // Set the small card 
 
             cardImg.style.color = fontColor;
             cardImg.style.background = backGroundColor;
@@ -326,16 +310,13 @@ function setBibleCard(userName) {
             downloadImg.style.color = fontColor;
 
             downloadBackImg.src = imgFile;
-            downloadBackImg.style.marginTop = "129px"
+            downloadBackImg.style.marginTop = "20%"
             downloadBackImg.style.width = "60px"
             downloadBackImg.style.height = "60px"
-            downloadCardText.style.paddingTop = '200px';
+            downloadCardText.style.height = '80%'
+            downloadCardText.style.paddingTop = '40%';
 
-        } else { // Logo top 40 bottum 60
-
-            cardBible.style.marginBottom = "5%";
-            downloadCardBible.style.marginTop = "13%";
-
+        } else { // Set the large card 
             cardImg.style.color = fontColor;
             cardImg.style.background = backGroundColor;
             
@@ -349,8 +330,8 @@ function setBibleCard(userName) {
             downloadImg.style.color = fontColor;
             downloadImg.style.background = backGroundColor;
             
-            downloadCardText.style.paddingTop = '50%';
-            downloadCardText.style.paddingBottom = '15%';
+            downloadCardText.style.paddingTop = '40%';
+            downloadCardText.style.paddingBottom = '7%';
             
             if (imgFile.includes("6-")) {
                 smallCardIcon.style.position = "absolute";
@@ -363,10 +344,16 @@ function setBibleCard(userName) {
 
                 downloadBackImg.style.position = "absolute";
                 downloadBackImg.style.transform = "rotate(11.74deg)";
-                downloadBackImg.style.top = "-6%";
-                downloadBackImg.style.left = "-5%";
-                downloadBackImg.style.width = "604.11px";
-                downloadBackImg.style.height = "788.97";
+                downloadBackImg.style.top = "-53.79px";
+                downloadBackImg.style.left = "-16.05px";
+                if ( (0 < window.innerHeight) & (window.innerHeight<= 750)) {
+                    downloadBackImg.style.height = window.innerHeight + 149 + "px";
+                } else if ((750 <= window.innerHeight)& (window.innerHeight<= 900)) {
+                    downloadBackImg.style.height = window.innerHeight + 95 + "px";
+                } else {
+                    downloadBackImg.style.height = window.innerHeight + "px";
+                }
+                downloadBackImg.style.width = "auto";
                 downloadBackImg.style.objectFit = "center"; 
                 downloadBackImg.style.objectPosition = "center"; 
 
@@ -380,10 +367,14 @@ function setBibleCard(userName) {
                 smallCardIcon.style.objectPosition = "center";
 
                 downloadBackImg.style.position = "absolute";
-                downloadBackImg.style.left = "-60px";
-                downloadBackImg.style.top = "-50px";
+                downloadBackImg.style.left = "-75px";
+                downloadBackImg.style.top = "-20px";
+                if ( (0 < window.innerHeight) & (window.innerHeight<= 750)) {
+                    downloadBackImg.style.height = window.innerHeight + 77 + "px";
+                } else {
+                    downloadBackImg.style.height = window.innerHeight + "px";
+                }
                 downloadBackImg.style.width = "auto";
-                downloadBackImg.style.height = window.innerHeight + 120 + "px";
                 downloadBackImg.style.objectFit = "center";
                 downloadBackImg.style.objectPosition = "center"; 
                 
@@ -395,12 +386,12 @@ function setBibleCard(userName) {
                 smallCardIcon.style.height = "501px";
                 smallCardIcon.style.objectFit = "center"; 
                 smallCardIcon.style.objectPosition = "center"; 
-
                 downloadBackImg.style.position = "absolute";
-                downloadBackImg.style.top = "-24px";
-                downloadBackImg.style.left = "-18px";
+                downloadBackImg.style.left = "-13px";
+                downloadBackImg.style.top = "-52px";
+
+                downloadBackImg.style.height = window.innerHeight + 113 + "px";;
                 downloadBackImg.style.width = "auto";
-                downloadBackImg.style.height = window.innerHeight + 130 + "px";;
                 downloadBackImg.style.objectFit = "center";
                 downloadBackImg.style.objectPosition = "center"; 
 
@@ -414,10 +405,14 @@ function setBibleCard(userName) {
                 smallCardIcon.style.objectPosition = "center"; 
 
                 downloadBackImg.style.position = "absolute";
+                downloadBackImg.style.left = "-30px";
+                downloadBackImg.style.top = "-31px";
+                if ( (0 < window.innerHeight) & (window.innerHeight<= 750)) {
+                    downloadBackImg.style.height = window.innerHeight + 85 + "px";
+                } else {
+                    downloadBackImg.style.height = window.innerHeight + 55 + "px";
+                }
                 downloadBackImg.style.width = "auto";
-                downloadBackImg.style.height = window.innerHeight + 85 + "px";
-                downloadBackImg.style.top = "-38px";
-                downloadBackImg.style.left = "-38px";
                 downloadBackImg.style.objectFit = "center";
                 downloadBackImg.style.objectPosition = "center"; 
 
@@ -431,10 +426,14 @@ function setBibleCard(userName) {
                 smallCardIcon.style.objectPosition = "center"; 
 
                 downloadBackImg.style.position = "absolute";
-                downloadBackImg.style.top = "77px";
-                downloadBackImg.style.left = "23px";
+                downloadBackImg.style.left = "44px";
+                downloadBackImg.style.top = "38px";
+                if ( (0 < window.innerHeight) & (window.innerHeight<= 750)) {
+                    downloadBackImg.style.height = window.innerHeight + 39 + "px";
+                } else {
+                    downloadBackImg.style.height = window.innerHeight + "px";
+                }
                 downloadBackImg.style.width = "auto";
-                downloadBackImg.style.height = window.innerHeight + 39 + "px";;
                 downloadBackImg.style.objectFit = "center";
                 downloadBackImg.style.objectPosition = "center"; 
 
